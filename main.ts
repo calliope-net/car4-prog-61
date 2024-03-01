d7a2812c6f2a6d81caadf35335b6643d794e9be4
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-03-01 11:27:46 -0800

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,28 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     car4sender.programm6(
-    car4sender.programmBlock(80, 150, cm),
-    car4sender.programmBlock(80, 30, cm),
+    car4sender.programmSchritt(50, 150, cm),
     null,
     null,
+    car4sender.programmSchrittT(-40, 8, cm),
     null
     )
     car4sender.sendBuffer0_setBit(car4sender.eBufferBit.x80_MotorPower, true)
     car4sender.sendBuffer0_setBit(car4sender.eBufferBit.fahrenStrecke, true)
     car4sender.sendBuffer19()
+    basic.showString("A")
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     car4sender.programm6(
-    car4sender.programmBlock(80, 150, cm),
-    car4sender.programmBlock(80, 30, cm),
-    car4sender.programmBlock(-80, 30, cm),
-    car4sender.programmBlock(100, 150, cm),
-    car4sender.programmBlock(100, 150, cm)
+    car4sender.programmSchritt(80, 150, cm),
+    car4sender.programmSchritt(80, 30, cm),
+    car4sender.programmSchritt(-80, 30, cm),
+    car4sender.programmSchritt(100, 150, cm),
+    car4sender.programmSchritt(100, 150, 29)
     )
     car4sender.sendBuffer0_setBit(car4sender.eBufferBit.x80_MotorPower, true)
     car4sender.sendBuffer0_setBit(car4sender.eBufferBit.fahrenStrecke, true)
     car4sender.sendBuffer19()
+    basic.showString("B")
 })
 input.onButtonEvent(Button.A, input.buttonEventValue(ButtonEvent.Hold), function () {
     cm += -1
