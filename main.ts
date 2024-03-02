7d520d5f703a33922237aa74ef0e67905cbbed70
calliope-net <141074597+calliope-net@users.noreply.github.com>
2024-03-02 12:41:53 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,9 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     car4sender.programm6(
-    car4sender.programmSchritt(50, 150, cm),
+    car4sender.programmSchritt(50, 90, 20),
     null,
     null,
-    car4sender.programmSchrittT(-40, 6, cm),
+    null,
     null
     )
     car4sender.sendProgramm19()
@@ -11,28 +11,18 @@ input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
 })
 input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
     car4sender.programm6(
-    car4sender.programmSchritt(80, 150, cm),
-    car4sender.programmSchritt(80, 30, cm),
-    car4sender.programmSchritt(-80, 30, cm),
-    car4sender.programmSchritt(100, 150, cm),
-    car4sender.programmSchritt(100, 150, cm)
+    car4sender.programmSchritt(80, 150, 250),
+    car4sender.programmSchritt(80, 30, 250),
+    car4sender.programmSchritt(-80, 30, 250),
+    car4sender.programmSchritt(100, 150, 250),
+    car4sender.programmSchritt(100, 150, 30)
     )
     car4sender.sendProgramm19()
     basic.showString("B")
-})
-input.onButtonEvent(Button.A, input.buttonEventValue(ButtonEvent.Hold), function () {
-    cm += -1
-    basic.showNumber(cm)
-})
-input.onButtonEvent(Button.B, input.buttonEventValue(ButtonEvent.Hold), function () {
-    cm += 1
-    basic.showNumber(cm)
 })
 function Konfiguration () {
     car4sender.comment("calliope-net/car4-prog-61")
     car4sender.comment("1 Erweiterung laden:")
     car4sender.comment("calliope-net/car4-sender")
 }
-let cm = 0
 car4sender.beimStart(240)
-cm = 250
